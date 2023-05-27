2f5ed744cceeecdc83346143acb0172666a4b5bd
Frumentius <frumentius_sonic@yahoo.com>
2023-05-27 21:51:51 +0700

    Commit pertama dan membuat komponen pertama 'Hello World'

---
+++ src/app/HelloWorld/hello-world.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  templateUrl: 'hello-world.component.html'
+})
+export class HelloWorldComponent {
+  // The code in this class drives the component's behavior.
+}
